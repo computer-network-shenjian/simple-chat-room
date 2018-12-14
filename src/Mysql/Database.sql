6650efe260307d40cb400a3ce60603868875174f
cyanic <Cyanic.yangyu@gmail.com>
2018-12-15 06:59:08 +0800

    finish most of the function

--- src/Mysql/Database.sql
+++ src/Mysql/Database.sql
@@ -5,9 +5,10 @@ use Simple_chat_room;
 
 drop table if exists account;
 create table account (
-username varchar(30) not null,
+username varchar(30) BINARY not null,
 password varchar(30) not null default '123456'
 );
+alter table account add primary key(username);
 
 insert into account(username) values('Cyanic');
 insert into account(username) values('Twofyw');
@@ -17,5 +18,9 @@ insert into account(username) values('novatez');
 drop table if exists history;
 create table history (
 message_id bigint unsigned,
+username_main varchar(30) BINARY not null,
+username_sub varchar(30) BINARY not null,
 message_info varchar(100) not null default 'error'
 );
+
+alter table history add primary key(message_id);
