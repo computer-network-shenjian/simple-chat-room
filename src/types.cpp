21266f6380cefa52a15e8062a557ead7e1d52c00
Twofyw Yang <ywywywx@gmail.com>
2018-12-19 16:44:59 +0800

    update

--- src/types.cpp
+++ src/types.cpp
@@ -2,7 +2,7 @@
 
 class CircularQueue {
     uint8_t *data;
-    size_t num_free_bytes;
+    size_t _num_free_bytes;
 
 public:
     CircularQueue(size_t size);
@@ -10,6 +10,8 @@ public:
 
     bool enqueue(const vector<uint_8> &v);
     vector<uint_8> dequeue(size_t size);
+
+    // Also requires a getter method for _num_free_bytes here.
 
 private:
 
