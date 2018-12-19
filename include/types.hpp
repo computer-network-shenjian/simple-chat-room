49919c8ebac37eb9b6b9bdfd63f70a287c0fed4b
Twofyw Yang <twofyw@tongji.edu.cn>
2018-12-19 09:35:35 +0800

    Update to camel naming

--- include/types.hpp
+++ include/types.hpp
@@ -1,21 +1,22 @@
-enum class packet_type: uint8_t {
-    info = 0x00,
-    infoRespond = 0x01,
-    passwd = 0x02,
-    passwdRespond = 0x03,
-    refuse = 0x04,
-    conf = 0x05,
-    historyUserName = 0x06,
-    history = 0x07,
-    syncEnd = 0x08,
-    textUser = 0x09,
-    text = 0x0A,
-    fileName = 0x0B,
-    fileInProgress = 0x0C,
-    groupTextUserlist = 0x0D,
-    fileEnd = 0x0E
+enum class PacketType: uint8_t {
+    Info = 0x00,
+    InfoRespond = 0x01,
+    Passwd = 0x02,
+    PasswdRespond = 0x03,
+    Refuse = 0x04,
+    Conf = 0x05,
+    HistoryUserName = 0x06,
+    History = 0x07,
+    SyncEnd = 0x08,
+    TextUsername = 0x09,
+    Text = 0x0A,
+    FileName = 0x0B,
+    FileInProgress = 0x0C,
+    GroupTextUserlist = 0x0D,
+    FileEnd = 0x0E
 };
-struct data_packet {
-    packet_type type;
+
+struct DataPacket {
+    Packet_type type;
     std::vector<uint8_t> data;
 };
