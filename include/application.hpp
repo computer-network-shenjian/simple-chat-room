784bbf0cc62450d9ca0a9212c97ffd8d4fe0c2d4
cyanic <Cyanic.yangyu@gmail.com>
2018-12-22 22:19:00 +0800

    application.hpp

--- include/application.hpp
+++ include/application.hpp
@@ -1,19 +1,18 @@
-#include "types.hpp"
+#include "../src/DatabaseConnection.cpp"
+#include "presentation.hpp"
 #include "client.hpp"
+#include "types.hpp"
+#include "log.h"
 #include <iostream>
 
-class ApplicationLayer {
-
-public:
-    ApplicationLayer() = default;
-    ~ApplicationLayer();
-
-    void MessageToApp(Client * client);
-private:
-    
-};
-
-void ApplicationLayer::MessageToApp(Client * client){
-	std::cout<<"test"<<std::endl;
-	return;
+class ApplicationLayer{
+        private:
+                DatabaseConnection *DatabaseConnectionPtr;
+                bool CheckUser(std::string user_name_);
+                bool CheckPasswd(std::String user_name_, std::string password_);
+        public:
+                ApplicationLayer();
+                ~ApplicationLayer();
+                void MessageToApp(Client *client);
 }
+
